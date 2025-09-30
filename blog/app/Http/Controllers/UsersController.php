aa42b53bdfd9665a802a9ba86cc966e60e4474dd
RafaelLujan1346 <rafael02lujan@gmail.com>
2025-09-30 16:37:52 -0600

    insertar registros

--- blog/app/Http/Controllers/UsersController.php
+++ blog/app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
@@ -14,7 +15,29 @@ class UsersController extends Controller
         return view('admin.users')
         ->with('usuarios',$data);
     }
-    public function createUsers(){
-     dd("si jalo");
+    public function createUsers(Request $request){
+     //dd($request->email);
+
+     //Reglas de validacion 
+     $request->validate([
+        'name'=>'required|min:3',
+        'nickname'=>'required|min:2|unique:users,nickname',
+        'email'=>'required|email|unique:users,email',
+        'password'=>'required|min:4',
+        'password2'=>'required|min:4|same:password'
+     ]);
+
+     //guardar registro
+        $user = new User();
+        $user->name = $request->name;
+        $user->nickname = $request->nickname;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->img = "default.jpg";
+        $user->save();
+
+        dd("Usuario creado");
+        return redirect()-back()->with('success','Usuario creado correctamente');
+
     }
 }
--- blog/resources/views/admin/users.blade.php
+++ blog/resources/views/admin/users.blade.php
@@ -9,6 +9,20 @@
     </button>
   </div>
 </div>
+@if(session('success'))
+<div class="alert alert-success">
+  {{session('success')}}
+</div>
+@endif
+@if($errors->any())
+<div class="alert alert-danger">
+  <ul>
+    @foreach($errors->all() as $error)
+    <li>{{$error}}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
 
 <div class="p-4">
   <table class="table">
@@ -32,7 +46,11 @@
         <td>{{$item->nickname}}</td>
         <td>{{$item->email}}</td>
         <td>*********</td>
-        <td><button Class="btn btn-danger">X</button></td>
+        <td><button class="btn btn-danger btnEliminar"
+        data-id="{{$item->id}}"
+         data-toggle="modal" data-target="#modalDelete">
+          <i class="fa fa-trash"></i>
+        </button></td>
       </tr>
       @endforeach
     </tbody>
@@ -53,28 +71,51 @@
           <div class="modal-body">
             <div class="form-group">
               <label for="exampleInputEmail1">Email</label>
-              <input type="email" class="form-control" id="Email1" aria-describedby="emailHelp">
+              <input value="{{ old('email')}}" name="email" type="email" class="form-control" id="Email1" aria-describedby="emailHelp">
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Nombre</label>
-              <input type="text" class="form-control" id="Name" aria-describedby="emailHelp">
+              <input value="{{ old('name')}}" name="name" type="text" class="form-control" id="Name" aria-describedby="emailHelp">
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Nickname</label>
-              <input type="text" class="form-control" id="Nickname" aria-describedby="emailHelp">
+              <input value="{{ old('nickname')}}" name="nickname" type="text" class="form-control" id="Nickname" aria-describedby="emailHelp">
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Password</label>
-              <input type="password" class="form-control" id="password" aria-describedby="emailHelp">
+              <input name="password" type="password" class="form-control" id="password" aria-describedby="emailHelp">
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">Confirm password</label>
-              <input type="password" class="form-control" id="password2" aria-describedby="emailHelp">
+              <input name="password2" type="password" class="form-control" id="password2" aria-describedby="emailHelp">
             </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="modalDelete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="/dashboard/users" method="POST">
+          @csrf
+          <div class="modal-body">
+            <h2>Desseas eliminar el registro?</h2>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-danger">Eliminar</button>
           </div>
         </form>
       </div>
@@ -87,5 +128,11 @@
 @section('scripts')
 <script>
   //alert("hola");
+  $(document).ready(function(){
+    $(".btnEliminar").on('click',function(event){
+      var id = $(this).data('id');
+      alert(id);
+    });
+  });
 </script>
 @endsection
